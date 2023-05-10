875cdc5e43a408fafbbdcfdaff981ad27e218cb9
ainurbrr <ainurbahtiarr@gmail.com>
2023-05-10 15:02:44 +0700

    update main

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/ainurbrr/go_mini-project_moh-ainur-bahtiar-rohman/tree/main/constants"
 	"github.com/ainurbrr/go_mini-project_moh-ainur-bahtiar-rohman/tree/main/routes"
 
 	config "github.com/ainurbrr/go_mini-project_moh-ainur-bahtiar-rohman/tree/main/config"
@@ -13,6 +17,13 @@ func main() {
 	db := config.Init()
 	e := echo.New()
 	routes.Routes(e, db)
+	port := os.Getenv("PORT")
 
-	e.Logger.Fatal(e.Start(":8080"))
+	if port == "" {
+		port = constants.DEFAULT_PORT
+	}
+
+	appPort := fmt.Sprintf(":%s", port)
+
+	e.Logger.Fatal(e.Start(appPort))
 }
